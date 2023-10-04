662d5809dd7bef8b9565ff8432568d451ba02386
stephen nene <stevekid705@gmail.com>
2023-10-04 17:21:30 +0300

    removed due to failure

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""MyList module"""
-
-
-class MyList(list):
-    """MyList class - Inherits from list"""
-    def print_sorted(self):
-        """Prints a sorted list"""
-        print(sorted(self))
